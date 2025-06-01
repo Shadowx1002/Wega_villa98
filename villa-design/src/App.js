8af07b510cb96cb3fa9cb97266ead8b6d5bdeacb
Shadowx1002 <nirodhakumarasinha1@gmail.com>
2025-06-02 00:59:39 +0530

    About.jsx
    About page

--- villa-design/src/App.js
+++ villa-design/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <About />
     </div>
   );
 }
